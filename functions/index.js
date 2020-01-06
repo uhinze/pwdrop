f372c526c889af54c6a122a95298bc192f12f140
Ulrich.Hinze <ulrich.hinze@cloudreach.com>
2020-01-06 18:07:30 +0100

    Add express.js

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+exports.api = functions.region('europe-west1').https.onRequest(app);
+
+app.get('/', (req, res) => {
+  res.send('Success');
+  });
