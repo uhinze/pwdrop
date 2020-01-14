a7dce90b19a21b7475ebefc05e8303b20c3631d6
Ulrich.Hinze <ulrich.hinze@cloudreach.com>
2020-01-14 09:15:11 +0100

    Add Not Found

--- public/get.js
+++ public/get.js
@@ -4,7 +4,8 @@ let retrievePw = new Vue({
     id: "",
     secret: "",
     secretInputType: "password",
-    secretInputButton: "fa-eye"
+    secretInputButton: "fa-eye",
+    found: true
   },
   methods: {
     hideReveal
@@ -13,9 +14,13 @@ let retrievePw = new Vue({
     let uri = window.location.search.substring(1);
     let params = new URLSearchParams(uri);
     this.id = params.get("id");
-    let response = await axios.get("/secret", {
-      params: { id: this.id }
-    });
-    this.secret = response.data.secret;
+    try {
+      let response = await axios.get("/secret", {
+        params: { id: this.id }
+      });
+      this.secret = response.data.secret;
+    } catch {
+      this.found = false;
+    }
   }
 });
